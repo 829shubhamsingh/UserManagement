d3d7c688f9d0e48a22ce880191e51814e2ac5201
Shubham Singh <829shubhamsingh@gmail.com>
2021-05-23 06:16:02 +0000

    Dialog Box addition

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -15,6 +15,20 @@ sap.ui.define([
                 localModel.loadData("model/mockData.json");
                 this.getView().setModel(localModel);
 
-			}
+            },
+            onHandleEditDetails:function(oEvent){
+                if (!this.oDefaultDialog){
+                    this.oDefaultDialog=sap.ui.xmlfragment(this.getView().getId(),"com.personal.usermanagement.Fragments.Edit",this)
+                    this.getView().addDependent(this.oDefaultDialog);
+                }
+                var selData=this.getView().getModel().getProperty(oEvent.getSource().getBindingContext().sPath)
+                var localModel=new sap.ui.model.json.JSONModel();
+                localModel.setData(selData);
+                this.getView().setModel(localModel,'selEmp');
+                this.oDefaultDialog.open();
+            },
+            onHandleCloseEditDialog:function(){
+                this.getView().byId('editDialog').close();
+            }
 		});
 	});
